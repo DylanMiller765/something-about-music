0b11c04a886a035ab715271780bdda34c872796d
Dylan Miller <dylanjaws185323@gmail.com>
2024-08-16 10:02:55 -0500

    update config and netlify

--- next.config.js
+++ next.config.js
@@ -12,11 +12,9 @@ const nextConfig = {
       ],
       formats: ['image/webp'],
     },
-    publicRuntimeConfig: {
+    env: {
       NEXT_PUBLIC_SECURE_POST_TOKEN: process.env.NEXT_PUBLIC_SECURE_POST_TOKEN,
     },
-    // Netlify specific settings
-    target: 'serverless',
     // Custom webpack config for Netlify
     webpack: (config, { isServer }) => {
       if (!isServer) {
