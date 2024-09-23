aee5e6da97575bf3efa6a1dc54f812c5694f677f
Dylan Miller <dylanjaws185323@gmail.com>
2024-09-23 13:54:23 -0500

    Fixed create post

--- pages/secure-create-post.js
+++ pages/secure-create-post.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Layout from '../../components/Layout'
+import Layout from '../components/Layout'
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
